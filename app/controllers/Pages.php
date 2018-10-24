d4d63b0a8054c5174dbe0d84f030965e416596aa
Your Name <you@example.com>
2018-10-24 19:04:03 +1300

    MVC

---
+++ app/controllers/Pages.php
@@ -0,0 +1,68 @@
+<?php
+     
+     include(APPROOT . '/helper/helperfunctions.php');
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    require APPROOT.'/vendor/phpmailer/phpmailer/src/Exception.php';
+    require APPROOT.'/vendor/phpmailer/phpmailer/src/PHPMailer.php';
+    require APPROOT.'/vendor/phpmailer/phpmailer/src/SMTP.php';
+   
+    class Pages extends Controller {
+      public function __construct() {
+        $this->pages = $this->model('_Pages'); //name your model
+      }
+      public function index() {
+        // variables for data or model functions go here
+        $emails = $this->pages->getAllMessages();
+        // add data or variables to the array using key-value pairs
+        $data = [
+          'emails' => $emails
+        ];
+        // call your view 
+        $this->view('pages/index', $data);
+      }
+      public function addMessages() {
+        
+        // variables for data or model functions go here
+     
+         $emails = $this->pages->getAllMessages();
+         
+        // add data or variables to the array using key-value pairs
+         $data = [
+          'emails' => $emails
+        ]; 
+        if(!empty($_POST['email']) && !empty($_POST['message'])) {
+          if($this->pages->addMessage($_POST['email'], $_POST['message'])) {
+
+
+            //How to use stack overflow :-) => https://stackoverflow.com/a/4963763
+            //Composer, install this => composer require phpmailer/phpmailer
+            $mail = new PHPMailer(true);
+            //Send mail using gmail
+            $mail->IsSMTP(); // telling the class to use SMTP
+            $mail->SMTPAuth = true; // enable SMTP authentication
+            $mail->SMTPSecure = "ssl"; // sets the prefix to the servier
+            $mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
+            $mail->Port = 465; // set the SMTP port for the GMAIL server
+            $mail->Username = GUSER; // Set the Constant in helper/startup.php
+            $mail->Password = GPASS; // Set the Constant in helper/startup.php
+            //Typical mail data
+            $mail->AddAddress($_POST['email'], 'Emerald');
+            $mail->SetFrom($_POST['email'], 'Emerald');
+            $mail->Subject = "Thank you :) We are looking forward to your next visit to the Official Website of FMH :)";
+            $mail->Body = $_POST['message'];
+            try{
+                $mail->Send();
+                header("Location: ".URLROOT."pages/index");
+            } catch(Exception $e){
+                //Something went bad
+                echo "<h1>Fail - </h1>" . $mail->ErrorInfo;
+            }
+          } 
+        } 
+        $this->view('pages/index', $data);
+      }
+    }
+
+?>
---
+++ app/models/_Pages.php
@@ -0,0 +1,39 @@
+<?php
+  //option, but easy: classname same as controller with leading underbar
+  class _Pages {
+    private $db;
+    public function __construct() {
+        $this->db = new Database;
+    }
+    // Any model functions go here
+    // Can return DB data or static
+    /////////////////////////////////////////////////
+    //////////////// EXAMPLES ///////////////////////
+    /////// YOU SHOULD DELETE THESE AFTER ///////////
+    /////////////////////////////////////////////////
+    //example: static data
+    public function title() {
+      return "Show All People";
+    }
+    //example: db data - select
+    public function getAllMessages() {
+      $this->db->query('SELECT * FROM tbl_emails');
+      return $this->db->resultSet();
+    }
+    //exaple: db data - insert
+    public function addMessage($em, $msg) {
+      
+      //Adding data to database
+      $this->db->query('INSERT INTO tbl_emails (EMAIL, MSG) VALUES (:em, :msg)');
+      //Binding Variables
+      $this->db->bind(':em', $em);
+      $this->db->bind(':msg', $msg);
+      //Return true or false, based on if query is successful or not
+      if($this->db->execute()) {
+          return true;
+      } else {
+          return false;
+      }
+    }
+  }
+?>
